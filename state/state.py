aa5979e0358aaf6dc90d01dc572e2776fb6a6ca8
Patel Rahul <rahulpatel16092005@gmail.com>
2026-06-22 18:08:20 +0530

    Define Software State

---
+++ state/state.py
@@ -0,0 +1,14 @@
+from typing import TypedDict
+
+class SoftwareState(TypedDict):
+    user_requirement: str
+
+    architecture: str
+
+    backend_code: str
+
+    frontend_code: str
+
+    review_report: str
+
+    test_cases: str
